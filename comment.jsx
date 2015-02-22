edbff6d4debd005309ceeb9b3b53bb66550b2169
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-02-22 12:12:28 -0500

    Getting to actual json data

--- comment.jsx
+++ comment.jsx
@@ -1,15 +1,20 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
         <div className="commentBox">
           <h1>Comments</h1>
-          <CommentList />
+          <CommentList data={this.props.data}/>
           <CommentForm />
         </div>
         );
   }
 });
-
+var converter = new Showdown.converter();
 var Comment = React.createClass({
   render: function() {
     return (
@@ -22,12 +27,19 @@ var Comment = React.createClass({
     );
   }
 });
+
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -44,6 +56,6 @@ var CommentForm = React.createClass({
 });
 
 React.render(
-    <CommentBox />,
+    <CommentBox data={data} />,
     document.getElementById('content')
     );
